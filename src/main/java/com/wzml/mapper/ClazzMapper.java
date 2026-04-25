6f7f428495968f0f662e95fad45becd110e708be
wzml11 <2706290264@qq.com>
2026-04-25 18:44:07 +0800

    Delete src directory

--- src/main/java/com/wzml/mapper/ClazzMapper.java
+++
@@ -1,9 +0,0 @@
-package com.wzml.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.wzml.pojo.Clazz;
-import org.apache.ibatis.annotations.Insert;
-
-public interface ClazzMapper extends BaseMapper<Clazz> {
-
-}
